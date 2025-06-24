a814fde521ba93c2b2b8d6baf81bba1cd9ee6475
shyun <exriik@gmail.com>
2025-06-24 16:50:41 +0900

    .md file modified

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Hello {
+	//main method
+	public static void main(String[] args) {
+		System.out.println("Welcom to JAVA!");
+		System.out.println("Hello~"); // 인터프리터 방식은 문장 하나 하나 읽고 실행, 세미콜론이 문장을 끝내는 부호기 때문에 없으면 오류 발생 
+	}
+}
--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -1,12 +1,16 @@
 package chapter02;
 
-public class HelloJava { //class에 들어가는 public은 필수는 아님, 메모리에서 누구나 사용가능하도록 public으로 설정
-
+public class HelloJava { 
+//class에 들어가는 public은 필수는 아님, 메모리에서 누구나 사용가능하도록 public으로 설정
 	//메인 메소드 - 실행을 위한 필수 메소드
 	public static void main(String[] args) { 
 		//args:저장소 이름, 메소드에서 변경 가능한 것은 args 밖에 없으나, 변경하는 일은 많지 않다  
-//		public, static 순서는 바뀌어도 되나, "void main(String[] args) {}" 순서는 바뀌면 안된다
-		System.out.println("Welcome to JavaWorld!!"); //구문을 실행하기 위해서는 main 안에 있어야 한다
+		//public, static 순서는 바뀌어도 되나, "void main(String[] args) {}" 순서는 바뀌면 안된다
+		//구문을 실행하기 위해서는 main 안에 있어야 한다
+		
+		System.out.println("Welcome to JavaWorld!!"); 
+		//System은 파스칼 케이스 이기 때문에 Class(java.base\java.lang\System.class)
+		//문자열은 "" 사이에만 들어감
 			
 	}
 
