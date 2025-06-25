ea6d69c3b5834ed8efb2990db95eef4a6eca8249
shyun <exriik@gmail.com>
2025-06-25 17:57:40 +0900

    250625

---
+++ src/chapter03/PracitceTest.java
@@ -0,0 +1,32 @@
+package chapter03;
+
+import java.util.Scanner;
+
+public class PracitceTest {
+	
+	static final int PUBNUM = 365;
+	public static void main (String[] args) {
+		
+//		//상단 출력부
+//		System.out.print("실수를 입력하시면 내림하여 정수를 출력 합니다. >");
+//		
+//		//입력부
+//		Scanner scan =
+//				new Scanner(System.in);
+//		double fnumber = scan.nextDouble();
+//		
+//		//값 출력부
+//		System.out.println((int)fnumber);
+//		
+//		//텍스트 입력 안내부
+//		System.out.print("텍스트를 입력하십시오. >");
+//		
+//		//입력부
+//		String textile = scan.next();
+//		
+//		System.out.println(textile);
+		System.out.println(PUBNUM+1);
+		
+	}
+
+}
